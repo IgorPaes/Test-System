b9685c62e1d7748b4c7033bedc4000eb48ebcc15
Andre Victor <andrevictor2102@hotmail.com>
2023-03-03 10:46:24 -0300

    initial commit

---
+++ src/projetoPi/main.java
@@ -0,0 +1,59 @@
+package projetoPi;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Scanner nm = new Scanner(System.in);
+		System.out.println("Projeto: System Fill");
+
+		System.out.println();
+
+		System.out.println("|**************Ben vindo Jogador****************|");
+
+		System.out.println();
+
+		System.out.println("+------------------------------------------------+");
+		System.out.println("|                 Escolha opção 1                  |");
+		System.out.println("|                 Escolha opção 2                  |");
+		System.out.println("|                 Escolha opção 3                  |");
+		System.out.println("|                 Escolha opção 4                  |");
+		System.out.println("+------------------------------------------------+");
+
+		System.out.println();
+		System.out.print("Digite sua opação: ");
+		String nomePersonagem;
+		int opcao = sc.nextInt();
+
+		while (opcao > 4) {
+
+			System.out.println("Digite a opção entre 1 e 4 corretamente:");
+
+			opcao = sc.nextInt();
+		}
+		switch (opcao) {
+
+		case 1:
+			System.out.println("Andre");
+			break;
+		case 2:
+			System.out.println("Digite o nome do seu personagem:");
+
+			nomePersonagem = nm.nextLine();
+			System.out.println(nomePersonagem);
+			break;
+		case 3:
+			System.out.println("Ygor");
+			break;
+		case 4:
+			System.out.println("Gui");
+			break;
+
+		}
+
+	}
+
+}
