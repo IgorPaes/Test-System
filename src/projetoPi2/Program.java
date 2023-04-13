89cd5cc389d9fca936027e4a27dfd2fe1d604d46
newandre <63376750+newandre@users.noreply.github.com>
2023-04-13 20:46:13 -0300

    Ttt
    igor vai usar

---
+++ src/projetoPi2/Program.java
@@ -0,0 +1,65 @@
+package projetoPi2;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int valorNota =0 ;
+		int somaNota =+ valorNota;
+		
+		System.out.println("+-----------------------------------------------+");
+		System.out.println("|                                               |");
+		System.out.println("|                 SEJA BEM - VINDO              |");
+		System.out.println("|                                               |");
+		System.out.println("+-----------------------------------------------+\n");
+		
+		System.out.println("******* Prova de Logica de Programação (JAVA) *******\n");
+
+		
+		
+		
+		System.out.println("Você é Professor ou Aluno?/n");
+
+		System.out.println("1) Professor");
+		
+		System.out.println("2) Aluno");
+		
+		int pergunta = sc.nextInt();
+		
+		switch(pergunta) { 
+		
+		case 1:
+			System.out.println("Opa, voce podera alterar algumas regras da prova.\n"
+					+ "Por exemplo: "
+					+ "Quanto vale cada pergunta e quantas tentativas o aluno podera realizar a prova.");
+			
+			break;
+		
+		case 2: System.out.println("Boa sorte aluno");
+		break;
+		}
+		
+		if (pergunta == 1) {
+			
+			System.out.println("Defina quanto vale cada pergunta, teremos um total de 5 questões");
+		  valorNota = sc.nextInt();
+		} 
+		
+		System.out.println();
+	
+	    System.out.println("Digite o seu nome:");
+		
+		String nomeAluno = sc.nextLine();
+		
+		System.out.printf("Bom, %s iremos começar a prova e desejo boa sorte *_*\n", nomeAluno);
+		
+		
+		
+		sc.close();
+	}
+
+}
