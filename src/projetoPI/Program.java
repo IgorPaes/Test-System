752066a6fdb50895f2fd140b52ee3df5aaa60055
Igor <88003618+IgorPaes@users.noreply.github.com>
2023-05-04 20:17:34 -0300

    Primeiro Commit

--- src/projetoPI/Program.java
+++ src/projetoPI/Program.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 
 public class Program {
 
-	// asksjjsasdasdasdassdasd
+	static Scanner sc = new Scanner(System.in); 
+
+	static char EscolhaMenu() {
+
+		char NumeroEscolhidoMenu = sc.next().charAt(0); 
+
+		System.out.print("\033\143");
+
+		return NumeroEscolhidoMenu;
+	} 
+
+	static char Auth() {
+
+
+
+	}
 
 	public static void main(String[] args) {
 
@@ -20,8 +35,8 @@ public class Program {
 
 		System.out.println("1) Professor");
 		System.out.println("2) Aluno");
-
-		System.out.print("\033\143");
+		
+		char NumeroEscolhido = EscolhaMenu();
 
 	}
 }
