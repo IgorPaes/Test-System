8830880e88c31e5d7c489a9a842c6e7c9cefc22c
Igor <88003618+IgorPaes@users.noreply.github.com>
2023-05-04 20:33:11 -0300

    Inicio Menu

--- src/projetoPI/Program.java
+++ src/projetoPI/Program.java
@@ -13,30 +13,53 @@ public class Program {
 		System.out.print("\033\143");
 
 		return NumeroEscolhidoMenu;
-	} 
+	}
 
-	static char Auth() {
+	static void MenuProfessor(char EscolhaTipousuario) {
 
+		switch(EscolhaTipousuario) {
+			case '1':
+				System.out.println("A");
+			break;
 
+			case '2':
+				System.out.println("B");
+			break;
+		}
 
+	}
+
+	static double MenuProfessor() {
+
+		System.out.println();
+
+		return 1;
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println("+-----------------------------------------------+");
-		System.out.println("|                                               |");
-		System.out.println("|                SEJA BEM - VINDO               |");
-		System.out.println("|                                               |");
-		System.out.println("+-----------------------------------------------+\n");
+		char NumeroEscolhido;
+
+		do {
+
+			System.out.println("+-----------------------------------------------+");
+			System.out.println("|                                               |");
+			System.out.println("|                SEJA BEM - VINDO               |");
+			System.out.println("|                                               |");
+			System.out.println("+-----------------------------------------------+\n");
+			
+			System.out.println("******* Prova de Logica de Programação (JAVA) *******\n");
+
+			System.out.println("Você é um Professor ou um Aluno?");
+
+			System.out.println("1) Professor");
+			System.out.println("2) Aluno");
+
+			NumeroEscolhido = EscolhaMenu();
+
+		}while(NumeroEscolhido != '1' && NumeroEscolhido != '2');
+
+		MenuProfessor(NumeroEscolhido);
 		
-		System.out.println("******* Prova de Logica de Programação (JAVA) *******\n");
-
-		System.out.println("Você é um Professor ou um Aluno?");
-
-		System.out.println("1) Professor");
-		System.out.println("2) Aluno");
-		
-		char NumeroEscolhido = EscolhaMenu();
-
 	}
 }
