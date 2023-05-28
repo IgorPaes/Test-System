bf2660f9c3cd9695b75d25ab501fbb564ca79d8e
Igor <igor.paes88@gmail.com>
2023-05-28 01:01:45 -0300

    Inicio função embaralhar questões

--- src/projetoPI/Program.java
+++ src/projetoPI/Program.java
@@ -114,6 +114,7 @@ public class Program {
 		}
 
 		int qtdQuestoes = 0;
+		String listaQuestoesPronta[];
 		
 		switch(numeroEscolhidoProfessor) {
 			case 1:
@@ -129,7 +130,7 @@ public class Program {
 			break;
 
 			case 3:
-				
+				listaQuestoesPronta = AtivaDesativaQuestoesAleatorias();
 			break;
 
 			case 4:
@@ -184,70 +185,65 @@ public class Program {
 
 	}
 	
-	static String[][] questoesProva() {
+	static String[] questoesProva() {
 
-		String listaQuestoesRespostas[][] = new String[2][10];
+		String listaQuestoesRespostas[] = new String[10];
 		
-		// PERGUNTAS
-		listaQuestoesRespostas[0][0] = "Questão  : Qual dos seguintes tipos de dados em Java representa números inteiros sem casas decimais?";
-		listaQuestoesRespostas[0][1] = "Questão  : Qual é a saída deste código Java?\n int x = 5;\nSystem.out.println(x++);";
-		listaQuestoesRespostas[0][2] = "Questão  : Em Java, qual é a palavra-chave utilizada para definir uma classe?";
-		listaQuestoesRespostas[0][3] = "Questão  : Qual é a estrutura de controle em Java utilizada para repetir um bloco de código várias vezes?";
-		listaQuestoesRespostas[0][4] = "Questão  : O que o seguinte código Java faz?\n String nome = 'João'\nSystem.out.println(nome.length())";
-		listaQuestoesRespostas[0][5] = "Questão  : Em Java, qual é a forma correta de declarar um array de inteiros chamado 'numeros' com tamanho 5?";
-		listaQuestoesRespostas[0][6] = "Questão  : O que o seguinte código Java faz?\nint resultado = Math.abs(-10);\nSystem.out.println(resultado);";
-		listaQuestoesRespostas[0][7] = "Questão  : Qual é a forma correta de escrever um comentário de várias linhas em Java?";
-		listaQuestoesRespostas[0][8] = "Questão  : Em Java, como você chama um método de uma classe 'MinhaClasse'?";
-		listaQuestoesRespostas[0][9] = "Questão  : Qual é a saída deste código Java? int[] numeros = {1, 2, 3, 4, 5}\nfor (int i = 0; i < numeros.length; i++) {\nSystem.out.print(numeros[i] + ' ')\n}";
-	
-
-		// ALTERNATIVAS
-		listaQuestoesRespostas[1][0] = 
+		listaQuestoesRespostas[0] = "Questão  : Qual dos seguintes tipos de dados em Java representa números inteiros sem casas decimais?" +
 		"A) int 1 \n" + /*correta */
 		"B) float 2 \n" + 
 		"C) double 3 \n" + 
 		"D) String 4 \n";
-		listaQuestoesRespostas[1][1] = 
+		
+		listaQuestoesRespostas[1] = "Questão  : Qual é a saída deste código Java?\n int x = 5;\nSystem.out.println(x++);" +
 		"A)  4 \n" + 
 		"B)  5 \n" + /*correta*/
 		"C)  6 \n" + 
 		"D)  O código resultará em um erro de compilação \n";
-		listaQuestoesRespostas[1][2] = 
+		
+		listaQuestoesRespostas[2] = "Questão  : Em Java, qual é a palavra-chave utilizada para definir uma classe?" +
 		"A)  class \n" + /*correta*/
 		"B)  public \n" + 
 		"C)  static \n" + 
 		"D)  void \n";
-		listaQuestoesRespostas[1][3] = 
+		
+		listaQuestoesRespostas[3] = "Questão  : Qual é a estrutura de controle em Java utilizada para repetir um bloco de código várias vezes?" +
 		"A)  if \n" + 
 		"B)  switch \n" + 
 		"C)  while \n" + 
 		"D)  for \n"; /*correta*/
-		listaQuestoesRespostas[1][4] = 
+		
+		listaQuestoesRespostas[4] = "Questão  : O que o seguinte código Java faz?\n String nome = 'João'\nSystem.out.println(nome.length())" +
 		"A)  Imprime o nome 'João' \n" + 
 		"B)  Retorna o tamanho do nome 'João' \n" + /*correta*/
 		"C)  Retorna a posição da letra 'o' no nome 'João' \n" + 
 		"D)  O código resultará em um erro de compilação \n";
-		listaQuestoesRespostas[1][5] = 
+		
+		listaQuestoesRespostas[5] = "Questão  : Em Java, qual é a forma correta de declarar um array de inteiros chamado 'numeros' com tamanho 5?" +
 		"A)  int[] numeros = new int[5]; \n" + /*correta*/
 		"B)  int[] numeros = {1, 2, 3, 4, 5}; \n" + 
 		"C)  int numeros[] = new int[5]; \n" + 
 		"D)  int numeros[] = {1, 2, 3, 4, 5}; \n";
-		listaQuestoesRespostas[1][6] = 
+		
+		listaQuestoesRespostas[6] = "Questão  : O que o seguinte código Java faz?\nint resultado = Math.abs(-10);\nSystem.out.println(resultado);" +
 		"A)  Calcula o valor absoluto de -10 \n" + /*correta */
 		"B)  Calcula a raiz quadrada de -10 \n" + 
 		"C)  Calcula o seno de -10 \n" + 
 		"D)  O código resultará em um erro de compilação \n";
-		listaQuestoesRespostas[1][7] = 
+		
+		listaQuestoesRespostas[7] = "Questão  : Qual é a forma correta de escrever um comentário de várias linhas em Java?" +
 		"A)  // Este é um comentário \n"   + 
 		"B)  /* Este é um comentário / \n" + /*correta*/
 		"C)  // Este é um comentário / \n" + 
 		"D)  /* Este é um comentário // \n";
-		listaQuestoesRespostas[1][8] = 
+		
+		listaQuestoesRespostas[8] = "Questão  : Em Java, como você chama um método de uma classe 'MinhaClasse'?" +
 		"A)  MinhaClasse.metodo(); \n" + /*correta*/								
 		"B)  metodo.MinhaClasse(); \n" + 
 		"C)  MinhaClasse.metodo; \n" + 
 		"D)  metodo.MinhaClasse; \n";
-		listaQuestoesRespostas[1][9] = 
+		
+		listaQuestoesRespostas[9] = "Questão  : Qual é a saída deste código Java? int[] numeros = {1, 2, 3, 4, 5}\nfor (int i = 0; i < numeros.length; i++) {\nSystem.out.print(numeros[i] + ' ')\n}" +
 		"A) 1 2 3 4 5 \n" + /*correto*/
 		"B) 5 4 3 2 1 \n" + 
 		"C) 1 1 1 1 1 \n" + 
@@ -301,39 +297,59 @@ public class Program {
 
 	}
 
+	static String[] AtivaDesativaQuestoesAleatorias() {
 
+		String listaQuestoesOriginal[] = questoesProva();
 
+		boolean btnAtivo = true;
+		int numeroEscolhido;
 
+		do {
+				
+			System.out.println("As questões estão organizadas de forma aleatoria.");
+			System.out.println("Digite 1 para DESATIVAR o modo de questões aleatórias.");
+			System.out.println("1) Desativar");
+			System.out.println("2) Manter Ativa");
+			numeroEscolhido = recebeLimpaInt();
+			
+		}while(numeroEscolhido <= 0 || numeroEscolhido > 2);
 
+		if(numeroEscolhido == 1) {
+			
+			btnAtivo = false;
 
+		}
 
+		String listaQuestoesPronta[] = AleatorizarQuestoes(btnAtivo, listaQuestoesOriginal);
+		
+		return listaQuestoesPronta;
+	}
 
+	static String[] AleatorizarQuestoes(boolean ativadaDesativada, String listaQuestoesOriginal[]) {
 
+		String listaQuestoesPronta[] = new String[10];
+		
 
+		if(ativadaDesativada) {
 
+			Random random = new Random();
+			int embaralharQuestoes = 1 + random.nextInt(9);
+			
+			for(int i = 0; i < listaQuestoesOriginal.length; i++) {
 
+				listaQuestoesOriginal[i];
 
+			}
 
+		}else {
 
+			
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		}
+			
+		
+		return listaQuestoesPronta;
+	}
 
 	static void menuInicial() {
 
